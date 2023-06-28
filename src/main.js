af37b47bd7f987e1f3bc73f32a32704f0e2d5dfc
X-chaochao <xing_chaochao@sina.com>
2023-06-28 23:50:40 +0800

    数据大屏初始话

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './assets/styles/global.scss'
+import dataV from '@jiaminghi/data-view'
+
 Vue.config.productionTip = false
+
+// 将自动注册所有组件为全局组件
+
+Vue.use(dataV)
 
 new Vue({
   router,
---
+++ src/views/LargeScreenVisualization/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+
+      <dv-full-screen-container class="container">
+        <dv-loading v-if="loading" class="loading">Loading...</dv-loading>
+        <dv-border-box-11 class="containerBorder" title="智慧大屏">
+          <div class="content">
+            <div class="top">
+              dsadsad
+            </div>
+            <div class="footer">
+              dsadsd
+            </div>
+          </div>
+        </dv-border-box-11>
+      </dv-full-screen-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LargeScreenVisualization',
+  data () {
+    return {
+      loading: false
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+  width: 100%;
+  background-color: #000;
+  color: #fff;
+
+  .loading {
+    color: #fff;
+  }
+  .containerBorder {
+    height: 100%;
+    width: 100%;
+    .content {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      .top {
+        flex: 2;
+        //background-color: red;
+      }
+      .footer {
+        flex: 1;
+        //background-color: green;
+      }
+    }
+  }
+
+}
+</style>
